18dff79becb2c316d2262866e8f2e57c4da461a7
Ziyad <zdn313@yahoo.com>
2022-09-20 17:27:57 -0500

    most pawn constraints complete. Need to figure out why didpass is not returning bool value (error handling). Once finished, I can work on the other piece constraints and begin final refactoring process.

--- chess2.py
+++ chess2.py
@@ -11,6 +11,8 @@
     
 #only 3 pieces of starting information to create the whole game
 
+
+
 board = ['.', 'P', 'R', 'N', 'B','Q','K']
 notation_letter= ['A','B','C','D','E','F','G','H']
 notation_number = ['1', '2', '3', '4', '5', '6', '7','8']  
@@ -31,20 +33,39 @@ for i in reversed(range(8)):
     continue
 
 
-#keeos record of pieces with respect to which side they belong to. Important for piece interaction logic
+#keeps record of pieces with respect to which side they belong to. Important for piece interaction logic
 white_dictionary = {}
 black_dictionary = {}
 
 
 def gameplay():
-    while True:
+    game = True
+   
+    while game == True:
+        #didpass = True
+    #while True:
         player_one = str(input('\n'+'Player 1:  ')).upper()
         print('\n')
-        give_board(usermove=True, player = player_one, colordict=white_dictionary)
+        didpass = give_board(usermove=True, player = player_one, colordict=white_dictionary) #bool returning None
+        print(didpass)
 
-        player_two = str(input('\n'+'Player 2:  ')).upper()
-        print('\n')
-        give_board(usermove=True, player = player_two, colordict=black_dictionary)
+        if didpass == False:
+            print('Please enter a valid move')
+            continue
+        #elif didpass == True:
+            break
+
+
+        while True:
+            player_two = str(input('\n'+'Player 2:  ')).upper()
+            print('\n')
+            didpass = give_board(usermove=True, player = player_two, colordict=black_dictionary)
+            if didpass == False:
+                print('Please enter a valid move', end='')
+                continue
+            elif didpass == True:
+                break
+                
 
 
  #takes input from player and parses it to be manipulated in the dictionary
@@ -60,18 +81,32 @@ def playermove(player, colordict):
 
 
 
-
     '''compare the value of move_from to a piece in the dictionary. If they are the same, then call pieceMovement function. If they are not the same, handle exception'''
     '''or compare move_from literal to a key in the colordict. If they are the same, then call the pieceMOvement function. If not, handle exception'''
 
 
     for key, value in colordict.items():
         if key == move_from:
+            possible_move_to_list = piece_movement(coordinate_list, colordict, move_from, move_to)
+            didpass = True
+            break #piece_movement_list should be returned at this point 
+        else:
+            print('invalid1') 
+            didpass = False #needs to return to player input to give another attempt
+
+    '''for key, value in coordinate_list[0]: #8th rank #do not delete, en pessant checker (is it neccessary?)
+        if key == move_to:
             piece_movement(coordinate_list, colordict, move_from, move_to)
             break
         else:
-            print('invalid') 
-            #needs to return to player input to give another attempt
+            print('invalid')
+    for key, value in coordinate_list[7]: #1st rank
+        if key == move_to:
+            piece_movement(coordinate_list, colordict, move_from, move_to)
+            break
+        else:
+            print('invalid')'''
+        
 
 
 
@@ -80,19 +115,38 @@ def playermove(player, colordict):
         if move_from == key:
             tempcopy = coordinate_list[8-int(move_from[1])][key] 
             coordinate_list[8-int(move_from[1])][key] = board[0]+'  ' #replaces every moved from space with a dot, indicating empty square
-
+            didpass = True
+            break
+        else:
+            print('invalid2') 
+            didpass = False
+    #print(didpass)
     #changes dictionary values of move to to move from -- inherent piece capture mechanism
-    for key, val in coordinate_list[8-int(move_to[1])].items():
+    '''for key, val in coordinate_list[8-int(move_to[1])].items():
         if move_to == key:
-            coordinate_list[8-int(move_to[1])][key] = tempcopy #coordinate_list[8-int(move_from[1])][move_from]
-        
-              
+            coordinate_list[8-int(move_to[1])][key] = tempcopy #coordinate_list[8-int(move_from[1])][move_from]'''
+
+
+
+    if move_to in possible_move_to_list:
+        coordinate_list[8-int(move_to[1])][move_to] = tempcopy
+        didpass = True
+    elif move_to not in possible_move_to_list:
+        coordinate_list[8-int(move_from[1])][move_from]  = tempcopy
+        print('invalid4') 
+        didpass = False
+
+            
+
+    print(didpass)
     
-def coordinate_positions(startpos, usermove,player, colordict):  #create dictionary of coordinates for plain board
+    return didpass
+
+    
+def coordinate_positions(startpos, usermove,player, colordict, ):  #create dictionary of coordinates for plain board
 
     '''Inputs: 'startpos' (bool), 'usermove'(bool), 'player'(keyword)
     This function determines the position of the visual gameboard in relation to the ever updating dictionary definitions of positions'''
-
 
     #gives the starting position of a chess game and updates dictionaries relating to property and position of pieces
     if startpos == True:
@@ -140,7 +194,7 @@ def coordinate_positions(startpos, usermove,player, colordict):  #create diction
 
         playermove(player, colordict)
 
-    return coordinate_list #calls the updated dictionary and thus updates the visual board
+    return coordinate_list#calls the updated dictionary and thus updates the visual board
 
 
 
@@ -172,6 +226,8 @@ def give_board(startpos = False, usermove = False, player = False, colordict = w
     print('\n'+notation_letter_string)
 
 
+
+
 def update_board(player_one='', player_two=''):
     pass
               
@@ -189,7 +245,7 @@ def piece_movement(coordinate_list, colordict, movefrom, moveto):# how pieces sh
     #these functions should save the avaliable moves for any given position for every piece. The return value will be compared to the players move-to
     #back in the player_move function and if they match the game will proceed. If not, the input will be marked invalid and player will be given another
     #chance to input a valid move
-    def pawnMovement(movefrom, moveto, coordinate_list): #currently only works for white side #WIP
+    def pawnMovement(movefrom, moveto, coordinate_list): #WIP
         '''generate a list of avaliable moves given the circumstances'''
         print('pawn moved')
         #a) max 2 space headstart when leaving home rank
@@ -199,26 +255,40 @@ def piece_movement(coordinate_list, colordict, movefrom, moveto):# how pieces sh
         #e) promotion
         #f) can't move over own side
 
+        def blackorwhite(num):
+            if colordict == white_dictionary:
+                adder = num
+            elif colordict == black_dictionary:
+                adder = -num
+            return adder
+
+
         def diagcapture(diagdirection, side_key):
+            adder = blackorwhite(1)
             diagdirection = list(key_list[side_key])
             for i in diagdirection:
                 if i == diagdirection[1]:
-                    diag_num = int(diagdirection[1]) + 1
+                    diag_num = int(diagdirection[1]) + adder
                     diagdirection = diagdirection[0] + str(diag_num)
                     possible_move_to_list.append(diagdirection)
+            return None
 
+                                    
 
         possible_move_to_list = []
 
         #a & b
         for key in coordinate_list[8-int(movefrom[1])]:
             if movefrom == key:
-                adder = 2
+                adder = blackorwhite(2)
                 while adder != 0:
                     potential_move_counter_filter = int(movefrom[1]) + adder
                     newmovefrom = movefrom[0] + str(potential_move_counter_filter)
                     possible_move_to_list.append(newmovefrom)
-                    adder -= 1 
+                    if colordict == white_dictionary:
+                        adder -= 1
+                    elif colordict == black_dictionary:
+                        adder += 1
 
 
         #c
@@ -227,54 +297,76 @@ def piece_movement(coordinate_list, colordict, movefrom, moveto):# how pieces sh
         for row in coordinate_list:
             for key, value in row.items():
                 key_list.append(key)
-                value_list.append(value)
-        
+                value_list.append(value) 
 
-    
         for key in coordinate_list[8-int(movefrom[1])]:
             if movefrom == key:
                 current_key = key_list.index(key)
-                left_of_current_key = current_key - 1
-                right_of_current_key = current_key + 1
-
-                
+                adder = blackorwhite(1)
+                left_of_current_key = current_key - adder
+                right_of_current_key = current_key + adder
 
                 leftdiag = ''
                 diagcapture(leftdiag ,left_of_current_key)
-
-                '''leftdiag = list(key_list[left_of_current_key])
-                for i in leftdiag:
-                    if i == leftdiag[1]:
-                        leftdiag_num = int(leftdiag[1]) + 1
-                        leftdiag = leftdiag[0] + str(leftdiag_num)
-                        possible_move_to_list.append(leftdiag)'''
+                rightdiag = ''
+                diagcapture(rightdiag, right_of_current_key)
+            
                 
-
-                rightdiag = list(key_list[right_of_current_key])
-
-
-
-                rightdiag_num = int(rightdiag[1]) + 1
-                rightdiag = rightdiag[0] + str(rightdiag_num)
-                possible_move_to_list.append(rightdiag)
-
-
-
-                '''print('test one passed')
-                print(key_list)
-                #print(current_key)
-                #print(left_of_current_key)
-                print(key_list[left_of_current_key])
-                #print(right_of_current_key)
-                print(key_list[right_of_current_key])
-                print(key_list[current_key])
-                print(leftdiag)
-                print(rightdiag)'''
                                
         #d
-        #e
 
-        print(possible_move_to_list) #this will get returned eventually back up to where piece_movement was first called
+        #e
+        '''when pawn reaches opposeite back rank, it promotes to a piece of the players choosing'''
+
+
+        def enpessant(ranknum, pawntype, promotedpiece):
+            for key, value in coordinate_list[ranknum].items():
+                if moveto == key and colordict[movefrom] == pawntype:
+                    coordinate_list[0][key] = promotedpiece
+                    if pawntype == 'P' + '  ':
+                        print('white pawn promoted')
+                    elif pawntype == 'p' + '  ':
+                        print('black pawn promoted')
+            return None
+
+
+
+
+        '''for key, value in coordinate_list[0].items():
+            if moveto == key and colordict[movefrom] =='P' + '  ':
+                coordinate_list[0][key] = 'Q' + '  '
+                print('white pawn promoted')
+        for key, value in coordinate_list[7].items():
+            if moveto == key and colordict[movefrom] =='p' + '  ':
+                coordinate_list[7][key] = 'q' + '  '
+                print('black pawn promoted')'''
+
+        enpessant(0," 'P' + '  ' ", "'Q' + '  '")
+        enpessant(7, "'p' + '  '", "'q' + '  '")
+
+
+                
+
+
+
+            
+        '''for key, value in coordinate_list[0].items(): #8th rank of board
+            if colordict == white_dictionary  and coordinate_list[0][key] == 'P' + '  ':
+                print('white pawn ready for promotion')
+                #coordinate_list[key] = 'Q' + '  '''
+
+
+        '''for i in coordinate_list[0]: #tests
+            print(coordinate_list[0][i])'''
+
+
+
+
+    
+
+        #f
+
+        return possible_move_to_list #this will get returned eventually back up to where piece_movement was first called
 
        
     def RookMovement():
@@ -290,7 +382,7 @@ def piece_movement(coordinate_list, colordict, movefrom, moveto):# how pieces sh
 
    #validates the piece that the player wants to move
     if colordict[movefrom] =='P' + '  ' or colordict[movefrom] == 'p' + '  ':
-        pawnMovement(movefrom, moveto, coordinate_list)
+        possible_move_to_list = pawnMovement(movefrom, moveto, coordinate_list)
     elif colordict[movefrom] == 'R'+ '  'or colordict[movefrom] == 'r' + '  ':
         RookMovement()
     elif colordict[movefrom] == 'N'+ '  'or colordict[movefrom] == 'n' + '  ':
@@ -309,7 +401,7 @@ def piece_movement(coordinate_list, colordict, movefrom, moveto):# how pieces sh
 
 
    
-    #return movefrom
+    return possible_move_to_list
 
     
 
@@ -322,7 +414,9 @@ def errorHandling():
 
 give_board(startpos=True)
 
-print(coordinate_list)
+
+
+#print(coordinate_list)
 #print(white_dictionary)
 #print(black_dictionary)
 
